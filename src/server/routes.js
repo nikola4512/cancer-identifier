ad6484a80514ea01d423328f3ef8c281156b7ba4
nikola4512 <nikolaarinanda01@gmail.com>
2024-05-23 12:46:23 +0700

    first commit

---
+++ src/server/routes.js
@@ -0,0 +1,18 @@
+const postPredictHandler = require("../server/handler");
+
+const routes = [
+  {
+    path: "/predict",
+    method: "POST",
+    handler: postPredictHandler,
+    options: {
+      payload: {
+        allow: "multipart/form-data",
+        multipart: true,
+        maxBytes: 1048576, // default (1MB)
+      },
+    },
+  },
+];
+
+module.exports = routes;
---
+++ src/services/inferenceService.js
@@ -0,0 +1,34 @@
+const tf = require("@tensorflow/tfjs-node");
+const InputError = require("../exceptions/InputError");
+
+async function predictClassification(model, image) {
+  try {
+    const tensor = tf.node
+      .decodeJpeg(image)
+      .resizeNearestNeighbor([224, 224])
+      .expandDims()
+      .toFloat();
+
+    let result, suggestion;
+
+    const prediction = model.predict(tensor);
+    const score = await prediction.data();
+    const confidenceScore = Math.max(...score) * 100;
+
+    confidenceScore > 50 ? (result = "Cancer") : (result = "Non-cancer");
+
+    if (result === "Cancer") {
+      suggestion = "Segera konsultasi dengan dokter terdekat.";
+    }
+
+    if (result === "Squamous cell carcinoma") {
+      suggestion = "Kondisi anda aman.";
+    }
+
+    return { result, suggestion };
+  } catch (error) {
+    throw new InputError(`Terjadi kesalahan input: ${error.message}`);
+  }
+}
+
+module.exports = predictClassification;
